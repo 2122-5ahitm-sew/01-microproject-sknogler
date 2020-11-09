8add1055b7a5a9dca6d4c810deb61b575ee05806
sKnogler <simon.knogler@aon.at>
2020-11-09 15:29:53 +0100

    Entity Host added

---
+++ src/main/java/at/htl/eventmanager/entity/Host.java
@@ -0,0 +1,41 @@
+package at.htl.eventmanager.entity;
+
+public class Host {
+
+    private String name;
+    private long ssn;
+    private long id;
+
+    public Host(String name, long ssn, long id) {
+        this.name = name;
+        this.ssn = ssn;
+        this.id = id;
+    }
+
+    public Host() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
