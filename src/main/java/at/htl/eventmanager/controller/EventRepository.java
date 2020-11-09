61f2756c5813988a46256c5a3fd8813fe8cd935a
sKnogler <simon.knogler@aon.at>
2020-11-09 14:46:24 +0100

    EntityRepository added

--- src/main/java/at/htl/eventmanager/controller/EventRepository.java
+++ src/main/java/at/htl/eventmanager/controller/EventRepository.java
@@ -1,5 +1,39 @@
 package at.htl.eventmanager.controller;
+
+import at.htl.eventmanager.entity.Event;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class EventRepository {
 
+    private static EventRepository instance;
+    private final HashMap<Long, Event> events = new HashMap<>();
+
+    public static EventRepository getInstance() {
+        if (instance == null) {
+            instance = new EventRepository();
+        }
+
+        return instance;
+    }
+
+    // add & update
+    public Event save(Event event) {
+        events.put(event.getId(), event);
+        return event;
+    }
+
+    public void delete(Event product) {
+        events.remove(product.getId());
+    }
+
+    public Event findById(long id) {
+        return events.get(id);
+    }
+
+    public List<Event> findAll() {
+        return List.copyOf(events.values());
+    }
 
 }
