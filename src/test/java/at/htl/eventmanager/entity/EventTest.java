265f5e0928abbf2bf409bd5d7542240022fe4315
sKnogler <simon.knogler@aon.at>
2020-11-12 20:57:04 +0100

    HostTest implpemented

--- src/test/java/at/htl/eventmanager/entity/EventTest.java
+++ src/test/java/at/htl/eventmanager/entity/EventTest.java
@@ -7,4 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EventTest {
 
+    @Test
+    void toStringTest() {
+        final Event event = new Event("KonzertXY", "22.August.2021", 1);
+        assertThat(event.toString()).isEqualTo("Event{name='KonzertXY', date='22.August.2021', id=1}");
+    }
 }
